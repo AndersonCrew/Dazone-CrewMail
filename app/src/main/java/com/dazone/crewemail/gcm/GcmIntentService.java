605068df53365375183079822f8ce4975aa851f1
Do Anh Tam <pikakun19@gmail.com>
2022-05-11 15:12:18 +0700

    4.0.8 done fix setting notification

--- app/src/main/java/com/dazone/crewemail/gcm/GcmIntentService.java
+++ app/src/main/java/com/dazone/crewemail/gcm/GcmIntentService.java
@@ -1,6 +1,7 @@
 package com.dazone.crewemail.gcm;
 
 import static android.support.v4.app.NotificationCompat.PRIORITY_HIGH;
+import static android.support.v4.app.NotificationCompat.PRIORITY_LOW;
 
 import android.annotation.TargetApi;
 import android.app.IntentService;
@@ -12,7 +13,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.media.AudioAttributes;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
@@ -24,7 +24,6 @@ import android.text.Html;
 import android.text.TextUtils;
 import android.util.Log;
 
-import com.dazone.crewemail.DaZoneApplication;
 import com.dazone.crewemail.R;
 import com.dazone.crewemail.activities.ActivityMailDetail;
 import com.dazone.crewemail.event.ReloadListMail;
@@ -37,8 +36,6 @@ import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 import org.greenrobot.eventbus.EventBus;
 
-import java.util.Random;
-
 
 public class GcmIntentService extends IntentService {
 
@@ -46,16 +43,32 @@ public class GcmIntentService extends IntentService {
         super("GcmIntentService");
     }
 
-    private String channelId = "Abc 1231";
-    private String channelName = "AHUHHS";
     boolean isEnableSound = true, isEnableVibrate = true, isNewMail = true, isTime = true;
     private String strFromTime, strToTime;
+
+    private String channelId = "0011001";
+    private String channelName = "CrewMail 0011001";
+    private String channelIdNonSound = "0022002";
+    private String channelNameNonSound = "CrewMail 0022002";
+    private NotificationChannel channel1, channel2;
+
+    private NotificationCompat.Builder mBuilder;
+    private NotificationManager mNotificationManager;
 
     @Override
     protected void onHandleIntent(Intent intent) {
         Bundle extras = intent.getExtras();
         GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
         String messageType = gcm.getMessageType(intent);
+
+        /** GET PREFERENCES */
+        isEnableVibrate = new Prefs().getBooleanValue(Statics.KEY_PREFERENCES_NOTIFICATION_VIBRATE, true);
+        isEnableSound = new Prefs().getBooleanValue(Statics.KEY_PREFERENCES_NOTIFICATION_SOUND, true);
+        isNewMail = new Prefs().getBooleanValue(Statics.KEY_PREFERENCES_NOTIFICATION_NEW_MAIL, true);
+        isTime = new Prefs().getBooleanValue(Statics.KEY_PREFERENCES_NOTIFICATION_TIME, true);
+        strFromTime = new Prefs().getStringValue(Statics.KEY_PREFERENCES_NOTIFICATION_TIME_FROM_TIME, Util.getString(R.string.setting_notification_from_time));
+        strToTime = new Prefs().getStringValue(Statics.KEY_PREFERENCES_NOTIFICATION_TIME_TO_TIME, Util.getString(R.string.setting_notification_to_time));
+
 
         if (extras != null) {
             if (!extras.isEmpty()) {
@@ -92,10 +105,59 @@ public class GcmIntentService extends IntentService {
         }
     }
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            channel1 = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
+            channel2 = new NotificationChannel(channelIdNonSound, channelNameNonSound, NotificationManager.IMPORTANCE_LOW);
+        }
+        startForeground((int) System.currentTimeMillis(), getNotification());
+    }
+
+    public Notification getNotification() {
+        String channel;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+            channel = createChannel();
+        else {
+            channel = "";
+        }
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, channel).setSmallIcon(android.R.drawable.ic_menu_mylocation).setContentTitle("crewChat");
+        Notification notification = mBuilder
+                .setPriority(isEnableSound ? PRIORITY_HIGH : PRIORITY_LOW)
+                .setCategory(Notification.CATEGORY_SERVICE)
+                .build();
+
+
+        return notification;
+    }
+
+    @NonNull
+    @TargetApi(26)
+    private synchronized String createChannel() {
+        NotificationManager mNotificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        NotificationChannel mChannel = null;
+        String chanelId = isEnableSound ? channelId : channelIdNonSound;
+        if (isEnableSound) {
+            mChannel = channel1;
+        } else {
+            mChannel = channel2;
+        }
+
+        mChannel.enableLights(true);
+        mChannel.setLightColor(Color.BLUE);
+        if (mNotificationManager != null) {
+            mNotificationManager.createNotificationChannel(mChannel);
+        } else {
+            stopSelf();
+        }
+
+        return chanelId;
+    }
 
     private void ShowNotification(String title, String fromName, String content, String receivedDate, String toAddress, long mailNo, String mailBoxNo) {
-        NotificationManager mNotificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         /** PendingIntent */
         Intent intent = new Intent(this, ActivityMailDetail.class);
@@ -106,153 +168,139 @@ public class GcmIntentService extends IntentService {
         PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
                 intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        /** GET PREFERENCES */
-        isEnableVibrate = new Prefs().getBooleanValue(Statics.KEY_PREFERENCES_NOTIFICATION_VIBRATE, true);
-        isEnableSound = new Prefs().getBooleanValue(Statics.KEY_PREFERENCES_NOTIFICATION_SOUND, true);
-        isNewMail = new Prefs().getBooleanValue(Statics.KEY_PREFERENCES_NOTIFICATION_NEW_MAIL, true);
-        isTime = new Prefs().getBooleanValue(Statics.KEY_PREFERENCES_NOTIFICATION_TIME, true);
-        strFromTime = new Prefs().getStringValue(Statics.KEY_PREFERENCES_NOTIFICATION_TIME_FROM_TIME, Util.getString(R.string.setting_notification_from_time));
-        strToTime = new Prefs().getStringValue(Statics.KEY_PREFERENCES_NOTIFICATION_TIME_TO_TIME, Util.getString(R.string.setting_notification_to_time));
-        Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
-        Uri soundUriSilent = Uri.parse("android.resource://"
-                + getApplicationContext().getPackageName() + "/" + R.raw.silent_sound);
-
-        NotificationChannel mChannel;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            mChannel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
-            mChannel.setLightColor(Color.GRAY);
-            mChannel.enableLights(true);
+            final long[] vibrate = new long[]{1000, 1000, 1000, 1000, 1000};
+            final Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-            AudioAttributes audioAttributes = new AudioAttributes.Builder()
-                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                    .setUsage(AudioAttributes.USAGE_NOTIFICATION)
-                    .build();
-
-            mNotificationManager.getNotificationChannel(channelId).setSound(isEnableSound ? soundUri : soundUriSilent, audioAttributes);
-            mNotificationManager.createNotificationChannel( mChannel );
-        }
-
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(this, channelId)
-                        .setSmallIcon(R.drawable.small_icon_email)
-                        .setTicker(getString(R.string.the_new_mail_has_arrived))
-                        .setPriority(Notification.PRIORITY_MAX)
-                        .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.launcher_icon))
-                        .setContentTitle(fromName)
-                        .setContentText(title)
-                        .setAutoCancel(true)
-                        .setContentIntent(contentIntent);
-
-        final long[] vibrate = new long[]{1000, 1000, 1000, 1000, 1000};
+            NotificationChannel mChannel = isEnableSound ? channel1 : channel2;
+            mChannel.setShowBadge(true);
+            mNotificationManager.createNotificationChannel(mChannel);
 
 
-        if (isEnableVibrate) {
-            mBuilder.setVibrate(vibrate);
-            Vibrator v = (Vibrator) DaZoneApplication.getInstance().getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
-            v.vibrate(500);
-        } else {
-            final long[] noVibrate = new long[]{0, 0, 0, 0, 0};
-            mBuilder.setVibrate(noVibrate);
-            Vibrator v = (Vibrator) DaZoneApplication.getInstance().getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
-            v.vibrate(0);
-        }
+            String idChanel = isEnableSound ? channelId : channelIdNonSound;
+            mBuilder = new NotificationCompat.Builder(getApplicationContext(), idChanel);
+            mBuilder.setPriority(isEnableSound ? PRIORITY_HIGH : PRIORITY_LOW)
+                    .setChannelId(idChanel)
+                    .setAutoCancel(false)
+                    .setSmallIcon(R.drawable.small_icon_email)
+                    .setTicker(getString(R.string.the_new_mail_has_arrived))
+                    .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.launcher_icon))
+                    .setContentTitle(fromName)
+                    .setContentText(title)
+                    .setContentIntent(contentIntent);
 
-        // Check notification setting and config notification
-        mBuilder.setSound(isEnableSound ? soundUriSilent : soundUriSilent);
+            // Check notification setting and config notification
+            if (isEnableSound) {
+                mBuilder.setSound(soundUri);
+            } else {
+                mBuilder.setSound(null);
+            }
 
+            if (isEnableVibrate) {
+                mBuilder.setVibrate(vibrate);
+                Vibrator v = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+                v.vibrate(1000);
+            } else {
+                final long[] noVibrate = new long[]{0, 0, 0, 0, 0};
+                mBuilder.setVibrate(noVibrate);
+                Vibrator v = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+                v.vibrate(0);
+            }
 
-        NotificationCompat.BigTextStyle bigTextStyle
-                = new NotificationCompat.BigTextStyle();
+            mBuilder.setContentIntent(contentIntent);
 
-        /** STYLE BIG TEXT */
-        String bigText = "<font color='#878787'>" + title + "</font>";
-        if (!TextUtils.isEmpty(content.replaceAll("&nbsp;", "").trim())) {
-            bigText = bigText + "<br/>" + content;
-        }
+            Notification notification = mBuilder.build();
+            notification.number = 100;
 
-        bigTextStyle.bigText(Html.fromHtml(bigText));
-        bigTextStyle.setSummaryText(toAddress);
-        mBuilder.setStyle(bigTextStyle);
+            /** STYLE BIG TEXT */
+            NotificationCompat.BigTextStyle bigTextStyle = new NotificationCompat.BigTextStyle();
+            String bigText = "<font color='#878787'>" + title + "</font>";
+            if (!TextUtils.isEmpty(content.replaceAll("&nbsp;", "").trim())) {
+                bigText = bigText + "<br/>" + content;
+            }
 
-        //consider using setTicker of Notification.Builder
-        if (isNewMail) {
-            if (isTime) {
-                if (TimeUtils.isBetweenTime(strFromTime, strToTime)) {
-                    mNotificationManager.notify(new Random().nextInt(), mBuilder.build());
+            bigTextStyle.bigText(Html.fromHtml(bigText));
+            bigTextStyle.setSummaryText(toAddress);
+            mBuilder.setStyle(bigTextStyle);
+            //consider using setTicker of Notification.Builder
+            if (isNewMail) {
+                if (isTime) {
+                    if (TimeUtils.isBetweenTime(strFromTime, strToTime)) {
+                        mNotificationManager.notify((int) System.currentTimeMillis(), mBuilder.build());
+                        startForeground((int) System.currentTimeMillis(), notification);
+                    }
+                } else {
+                    mNotificationManager.notify((int) System.currentTimeMillis(), mBuilder.build());
+                    startForeground((int) System.currentTimeMillis(), notification);
                 }
+            }
+
+        } else {
+            final long[] vibrate = new long[]{1000, 1000, 0, 0, 0};
+            final Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+            mBuilder = new NotificationCompat.Builder(getApplicationContext());
+            mBuilder.setPriority(isEnableSound ? PRIORITY_HIGH : PRIORITY_LOW)
+                    .setAutoCancel(false)
+                    .setSmallIcon(R.drawable.small_icon_email)
+                    .setTicker(getString(R.string.the_new_mail_has_arrived))
+                    .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.launcher_icon))
+                    .setContentTitle(fromName)
+                    .setContentText(title)
+                    .setContentIntent(contentIntent);
+
+            // Check notification setting and config notification
+            if (isEnableSound) {
+                mBuilder.setSound(soundUri);
             } else {
-                mNotificationManager.notify(new Random().nextInt(), mBuilder.build());
+                mBuilder.setSound(null);
+            }
+
+            if (isEnableVibrate) {
+                mBuilder.setVibrate(vibrate);
+                Vibrator v = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+                v.vibrate(1000);
+            } else {
+                final long[] noVibrate = new long[]{0, 0, 0, 0, 0};
+                mBuilder.setVibrate(noVibrate);
+                Vibrator v = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+                v.vibrate(0);
+            }
+
+            mBuilder.setContentIntent(contentIntent);
+
+            Notification notification = mBuilder.build();
+            notification.number = 100;
+
+
+
+            /** STYLE BIG TEXT */
+            NotificationCompat.BigTextStyle bigTextStyle = new NotificationCompat.BigTextStyle();
+            String bigText = "<font color='#878787'>" + title + "</font>";
+            if (!TextUtils.isEmpty(content.replaceAll("&nbsp;", "").trim())) {
+                bigText = bigText + "<br/>" + content;
+            }
+
+            bigTextStyle.bigText(Html.fromHtml(bigText));
+            bigTextStyle.setSummaryText(toAddress);
+            mBuilder.setStyle(bigTextStyle);
+            //consider using setTicker of Notification.Builder
+            if (isNewMail) {
+                if (isTime) {
+                    if (TimeUtils.isBetweenTime(strFromTime, strToTime)) {
+                        mNotificationManager.notify((int) System.currentTimeMillis(), mBuilder.build());
+                        startForeground((int) System.currentTimeMillis(), notification);
+                    }
+                } else {
+                    mNotificationManager.notify((int) System.currentTimeMillis(), mBuilder.build());
+                    startForeground((int) System.currentTimeMillis(), notification);
+                }
             }
         }
     }
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        /** GET PREFERENCES */
-        isEnableVibrate = new Prefs().getBooleanValue(Statics.KEY_PREFERENCES_NOTIFICATION_VIBRATE, true);
-        isEnableSound = new Prefs().getBooleanValue(Statics.KEY_PREFERENCES_NOTIFICATION_SOUND, true);
-        isNewMail = new Prefs().getBooleanValue(Statics.KEY_PREFERENCES_NOTIFICATION_NEW_MAIL, true);
-        isTime = new Prefs().getBooleanValue(Statics.KEY_PREFERENCES_NOTIFICATION_TIME, true);
-        strFromTime = new Prefs().getStringValue(Statics.KEY_PREFERENCES_NOTIFICATION_TIME_FROM_TIME, Util.getString(R.string.setting_notification_from_time));
-        strToTime = new Prefs().getStringValue(Statics.KEY_PREFERENCES_NOTIFICATION_TIME_TO_TIME, Util.getString(R.string.setting_notification_to_time));
-
-        startForeground(1, getNotification());
-    }
-
-    public Notification getNotification() {
-        String channel;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-            channel = createChannel();
-        else {
-            channel = "";
-        }
-
-        Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        Uri soundUriSilent = Uri.parse("android.resource://"
-                + getApplicationContext().getPackageName() + "/" + R.raw.silent_sound);
-
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, channel).setSmallIcon(android.R.drawable.ic_menu_mylocation).setContentTitle("CrewMail");
-        // Check notification setting and config notification
-        mBuilder.setSound(isEnableSound ? soundUri : soundUriSilent);
-        Notification notification = mBuilder
-                .setPriority(PRIORITY_HIGH)
-                .setCategory(Notification.CATEGORY_SERVICE)
-                .build();
-
-        return notification;
-    }
-
-    @NonNull
-    @TargetApi(26)
-    private synchronized String createChannel() {
-        NotificationManager mNotificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-
-        Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        Uri soundUriSilent = Uri.parse("android.resource://"
-                + getApplicationContext().getPackageName() + "/" + R.raw.silent_sound);
-
-
-        NotificationChannel mChannel;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            mChannel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_HIGH);
-            mChannel.setLightColor(Color.GRAY);
-            mChannel.enableLights(true);
-
-            AudioAttributes audioAttributes = new AudioAttributes.Builder()
-                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                    .setUsage(AudioAttributes.USAGE_NOTIFICATION)
-                    .build();
-            mChannel.setSound(isEnableSound ? soundUri : soundUriSilent, audioAttributes);
-
-            if (mNotificationManager != null) {
-                mNotificationManager.createNotificationChannel( mChannel );
-            } else {
-                stopSelf();
-            }
-        }
-
-        return channelId;
-    }
 }
